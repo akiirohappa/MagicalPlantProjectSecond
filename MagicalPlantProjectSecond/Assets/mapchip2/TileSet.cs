c259f55db3e8cc99dcc70beab3bab1bcfb8c531b
akiirohappa <kihara.k2000@gmail.com>
2019-10-11 09:51:31 +0900

    Project(空)

---
+++ MagicalPlantProjectSecond/Assets/mapchip2/TileSet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class TileSet : MonoBehaviour
+{
+    [SerializeField] Tilemap[] tiles;
+    [SerializeField] TileBase tiletest;
+    [SerializeField] Vector3Int plantStart;
+    [SerializeField] Vector3Int plantEnd;
+    [SerializeField] Vector3Int pos;
+    [SerializeField] List<Vector3Int> plantFieldPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TilePlantFieldGet(tiles[0]);
+        PlantData p = new PlantData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TileGet(tiles[0]);
+    }
+    void TilePlantFieldGet(Tilemap t)
+    {
+        plantFieldPos = new List<Vector3Int>();
+        for(int x = plantStart.x;x <= plantEnd.x; x++)
+        {
+            for (int y = plantStart.y; y <= plantEnd.y; y++)
+            {
+                plantFieldPos.Add(new Vector3Int(x, y, 0));
+                t.SetTile(new Vector3Int(x, y, 0), tiletest);
+            }
+        }
+    }
+}
